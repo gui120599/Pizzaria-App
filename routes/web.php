e2cc5c60dd668ecd7cc691c4e97345e9b57d1720
Guilherme Marins dos Santos <guilherme.santos@hic.srv>
2024-09-11 15:46:28 -0300

    Primeiro Build

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/Pedido/{id}/Imprimir', [PDFController::class, 'pedidoPDF'])->name('pedido.imprimir');
